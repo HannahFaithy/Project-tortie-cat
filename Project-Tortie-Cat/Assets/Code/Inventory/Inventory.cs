8f11eb7bd0686b324ab987c71d3e8d91b9cdcfa3
Faithy_dot <hlfaithfull@gmail.com>
2024-04-05 07:53:26 +1030

    added things
    added scvriptable inventory and items. need to expand on both.

---
+++ Project-Tortie-Cat/Assets/Code/Inventory/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory/Inventory")]
+public class Inventory : ScriptableObject
+{
+    public List<Item> items = new List<Item>();
+
+    public void AddItem(Item item)
+    {
+        items.Add(item);
+    }
+
+    public void RemoveItem(Item item)
+    {
+        items.Remove(item);
+    }
+}
---
+++ Project-Tortie-Cat/Assets/Code/Scriptable objects/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public Sprite icon;
+    public int quantity;
+}
