c6d957afab36d47879a5272d7534945f6a75e19c
Faithy_dot <hlfaithfull@gmail.com>
2024-04-16 13:14:37 +0930

    revert commit

--- Project-Tortie-Cat/Assets/Code/Menu things/Inventory.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
-
-[CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory/Inventory")]
-public class Inventory : ScriptableObject
-{
-    public List<Item> items = new List<Item>();
-
-}
